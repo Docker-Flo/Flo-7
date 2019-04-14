2b8d0f1473ec0213df0c461e14313075fa0bb046
Will Hardcastle <me@fonix.online>
2019-04-14 16:40:36 +0100

    Final Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ echo "Modified Container Selected"
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-lftp -u rest,AntoherPassword -e "put zippy.zip;quit" 54.39.62.75
-
 #Old Backup Method 
-#zip -r Backup-$(date +"%Y-%m-%d").zip /home/container/*
-#ehco "Backup Complete"
+zip -r zippy.zip /home/container/*
+ehco "Backup Complete"
+
+lftp -u rest,AntoherPassword -e "put zippy.zip;quit" 54.39.62.75
 
 if [ $? -ne 0 ]; then
     echo "FLO7_CONTAINER_ERR: There was an error while attempting to run the start command."
