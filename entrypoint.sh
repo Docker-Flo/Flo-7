a506e3ee2cbcab780b0d6141bf9324a89745a9bc
Will Hardcastle <me@fonix.online>
2019-04-14 15:11:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,11 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 chmod 600 /home/container/magicpi
-sudo chmod 600 /home/container/magicpi
-rsync -avz -P -e "ssh -i /home/container/magicpi" /home/container/* root@54.39.62.75://home/another/ >> rsync.log
+rsync -avz -P -e 'ssh -i /home/container/magicpi' /home/container/* root@54.39.62.75://home/another/
 
 #Old Backup Method 
 #zip -r Backup-$(date +"%Y-%m-%d").zip /home/container/*
 #ehco "Backup Complete"
-
-# Run the Server
-${MODIFIED_STARTUP}
 
 if [ $? -ne 0 ]; then
     echo "FLO7_CONTAINER_ERR: There was an error while attempting to run the start command."
